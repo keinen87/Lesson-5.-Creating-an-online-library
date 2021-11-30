dae3cdcb5041a57d1dd40958dda62401d2d6df25
Никита <64539102+ruk228@users.noreply.github.com>
2021-11-30 20:45:37 +0400

    два столбца(тест)

--- render_website.py
+++ render_website.py
@@ -14,6 +14,8 @@ def get_rendered_page():
         autoescape=select_autoescape(['html', 'xml'])
     )
 
+    books_information = [{"filename": "Алиби   ", "author": "   ИВАНОВ Сергей", "image_name": "shots/239.jpg", "genres": ["Научная фантастика"]}]
+
     template = env.get_template('template.html')
     rendered_page = template.render(
         books_information=books_information,
